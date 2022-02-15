3803cb0621ba5f1402616d510f77e204a6232735
Pranshul Atri <pranshul19109193@gmail.com>
2022-02-15 15:00:20 +0530

    Made some changes to bye.c file

--- bye.c
+++ bye.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-#include <math.h>
-int main(void) {
-  int n,i,p=0;
-  printf("Enter a positive number \n");
-  scanf("%d",&n);
-  for (i=1;i<=n;i++){
-    if(n%i==0){
-      p++;
-    }
+#include<math.h>
+int main() {
+  int b,c,e,f,k;
+  k=1;
+  c=1;
+for (b=1 ; b<=4 ; b=b+1){
+    for(e=4;e>b;e=e-1){
+    printf(" ");
   }
-    if(p==2){
-      printf("The number entered is a prime number");
-    }
-    else{
-      printf("The number entered is not a prime number ");
-    }       
+  for (f=1;f<=b;f=f+1){
+    printf("%d  ",k);
+    k=k+1;
+  }
+  printf("\n");
+}
 }
