5e93fa3ef7baf3ad0dd364a7dc0a7f7a2ee70f9e
Pranshul Atri <pranshul19109193@gmail.com>
2022-02-15 17:30:15 +0530

    once again commiting

--- bye.c
+++ bye.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
-#include<math.h>
-int main() {
-  int b,c,e,f,k;
-  k=1;
-  c=1;
-for (b=1 ; b<=4 ; b=b+1){
-    for(e=4;e>b;e=e-1){
-    printf(" ");
-  }
-  for (f=1;f<=b;f=f+1){
-    printf("%d  ",k);
-    k=k+1;
-  }
-  printf("\n");
-}
+#include <math.h>
+int main(void) {
+  int n,previous,current,next;
+  printf("abcd");
+  scanf("%d",&n);
+  previous=0;
+  current=1;
+  next=1;
+  do{
+    printf("\n%d",previous);
+    previous=current;
+    current=next;
+    next=previous+current;
+    n=n-1;
+  }while(n!=0);
+     
 }
